28abe59a937a7cf723b2d51d105fee2a97175ec5
jianbinlu225 <dashuaiguolou@gmail.com>
2020-06-03 13:42:59 +0800

    first spring boot program 1.1

--- chapter1-1/src/main/java/com/example/chapter11/HelloController.java
+++ chapter1-1/src/main/java/com/example/chapter11/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.web;
+package com.example.chapter11;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
